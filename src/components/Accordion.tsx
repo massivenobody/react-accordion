62386326f8b00d633eddb685be3e68b971e83366
Hollow <91344624+massivenobody@users.noreply.github.com>
2025-03-28 16:36:05 -0500

    feat: implement expand/collapse functionality
    - Implement logic in Accordion to track which AccordionItem is open.
    - Pass relevant event handlers down to AccordionItems.
    - Show accordion content depending on isOpen.

--- src/components/Accordion.tsx
+++ src/components/Accordion.tsx
@@ -1,9 +1,22 @@
 import AccordionItem from "./AccordionItem";
 import { accordionData } from "../data/accordionData";
+import { useState } from "react";
 
 function Accordion() {
+    const [activeId, setActiveId] = useState<number | null>(null);
+
+    const handleToggle = (id: number) => {
+        setActiveId(activeId === id ? null : id);
+    }
+
     const accordionItems = accordionData.map((item) => (
-        <AccordionItem key={item.id} title={item.title} content={item.content} />
+        <AccordionItem
+            key={item.id}
+            title={item.title}
+            content={item.content}
+            isOpen={activeId === item.id}
+            onToggle={() => handleToggle(item.id)}
+        />
     ))
 
     return (
--- src/components/AccordionItem.tsx
+++ src/components/AccordionItem.tsx
@@ -1,13 +1,15 @@
-function AccordionItem({ title, content }: { title: string, content: string }) {
+function AccordionItem({ title, content, isOpen, onToggle }: { title: string, content: string, isOpen: boolean, onToggle: () => void }) {
 
     return (
-        <div className="accordion-item">
+        <div className="accordion-item" onClick={onToggle}>
             <div className="accordion-title">
                 <h2>{title}</h2>
             </div>
-            <div className="accordion-content">
-                <p>{content}</p>
-            </div>
+            {isOpen && (
+                <div className="accordion-content">
+                    <p>{content}</p>
+                </div>
+            )}
         </div>
     )
 }

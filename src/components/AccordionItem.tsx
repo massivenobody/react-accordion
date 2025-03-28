03bee5f27f244ff8d0ffc6b3f80928c61e5a6d2f
Hollow <91344624+massivenobody@users.noreply.github.com>
2025-03-28 16:40:06 -0500

    feat: add expand/collapse icon
    - Add Material UI as dependency.
    - Show ExpandMore and ExpandLess conditionally.
    - Modify accordion-title styling.

--- src/components/AccordionItem.tsx
+++ src/components/AccordionItem.tsx
@@ -1,9 +1,15 @@
+import { ExpandMore, ExpandLess } from "@mui/icons-material";
+
 function AccordionItem({ title, content, isOpen, onToggle }: { title: string, content: string, isOpen: boolean, onToggle: () => void }) {
 
     return (
         <div className="accordion-item" onClick={onToggle}>
             <div className="accordion-title">
                 <h2>{title}</h2>
+                {isOpen
+                    ? <ExpandLess />
+                    : <ExpandMore />
+                }
             </div>
             {isOpen && (
                 <div className="accordion-content">
